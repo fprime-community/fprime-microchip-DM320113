bd15ab8adfa1cd934fabb0f05b9a7404f0a46a05
dtriska <dtriska21@yahoo.com>
2024-07-17 15:32:46 -0700

    removed baremetal options used in development/testing

--- cmake/platform/sam_e70_xult.cmake
+++ cmake/platform/sam_e70_xult.cmake
@@ -7,10 +7,10 @@ set(FPRIME_PLATFORM "sam_e70_xult")
 set(CMAKE_SYSTEM_PROCESSOR cortex-m7)
 
 # Set the compiler flags
-set(CMAKE_C_FLAGS_INIT "-mprocessor=ATSAME70Q21B -D__SAME70Q21B__ -isystem ${XC32_DIR}/include -isystem ${CMAKE_CURRENT_LIST_DIR}/../sam_e70_xult_src/same70b/include -isystem ${CMAKE_CURRENT_LIST_DIR}/../sam_e70_xult_src/same70b/include_mcc -isystem ${CMAKE_CURRENT_LIST_DIR}/../sam_e70_xult_src/same70b/xc32/include")
-set(CMAKE_EXE_LINKER_FLAGS_INIT "-specs=${CMAKE_CURRENT_LIST_DIR}/../sam_e70_xult_src/same70b/xc32/ATSAME70Q21B/specs-ATSAME70Q21B -Wl,--defsym,_min_heap_size=0x2000")
+set(CMAKE_C_FLAGS_INIT "-mprocessor=ATSAME70Q21B -D__SAME70Q21B__ -isystem ${XC32_DIR}/include -isystem /opt/microchip/mplabx/v6.20/packs/Microchip/SAME70_DFP/4.10.248/same70b/xc32/include -isystem /opt/microchip/mplabx/v6.20/packs/Microchip/SAME70_DFP/4.10.248/same70b/include_mcc -isystem /opt/microchip/mplabx/v6.20/packs/Microchip/SAME70_DFP/4.10.248/same70b/include")
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-specs=/opt/microchip/mplabx/v6.20/packs/Microchip/SAME70_DFP/4.10.248/same70b/xc32/ATSAME70Q21B/specs-ATSAME70Q21B -Wl,--defsym,_min_heap_size=0x2000") # heap size can be reset, this is a placeholder
 
-# Prevent CMake from adding its own -mcpu and -mthumb flags
+# Prevent CMake from adding its own -mcpu and -mthumb flags which conflict with the specific flags required for the ATSAME70Q21B processor
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS_INIT}" CACHE STRING "Initial C FLAGS" FORCE)
 set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS_INIT}" CACHE STRING "Initial CXX FLAGS" FORCE)
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS_INIT}" CACHE STRING "Initial EXE LINKER FLAGS" FORCE)
@@ -21,8 +21,3 @@ include_directories(SYSTEM "${CMAKE_CURRENT_LIST_DIR}/types")
 # Remove POSIX
 set(FPRIME_USE_POSIX OFF)
 
-# Define the operating system type for embedded platform
-add_definitions(-DFW_BAREMETAL_SCHEDULER=1)
-
-# Specify the OS type include directive
-add_definitions(-DTGT_OS_TYPE_BAREMETAL)
